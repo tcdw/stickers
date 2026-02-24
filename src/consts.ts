6d3566bc0da67a39c8e2ef8b7765e5b99078c3be
tcdw <tcdw2011@gmail.com>
2026-02-24 20:34:26 +0800

    feat: about page

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_MENU = [
   { title: "首页", href: "/", target: "_self" },
-  { title: "人设", href: "#", target: "_self" },
+  { title: "关于", href: "/about", target: "_self" },
   { title: "壁纸", href: "#", target: "_self" },
   { title: "设计规范", href: "#", target: "_self" },
 ];
