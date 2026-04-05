c9c105d06421078346c004fa9019e1023b3c5413
tcdw <tcdw2011@gmail.com>
2026-04-05 21:10:33 +0800

    feat: hide wallpaper page for now

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_MENU = [
   { title: "首页", href: "/", target: "_self" },
   { title: "关于", href: "/about", target: "_self" },
-  { title: "壁纸", href: "/wallpapers", target: "_self" },
+  // { title: "壁纸", href: "/wallpapers", target: "_self" },
   { title: "设计规范", href: "/design", target: "_self" },
 ];
